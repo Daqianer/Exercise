18a2187b9292d0128b9dfefbdff871a8efd13f0a
daqianer <daqianer@daqianerdeMacBook-Pro.local>
2021-01-18 15:36:23 +0800

    P106

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React from "react";
-import Movies from "./components/movies";
+
 import { Route, Redirect, Switch } from "react-router-dom";
-import "./App.css";
+import NavBar from "./components/navbar";
+import Movies from "./components/movies";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
+import "./App.css";
 
 function App() {
   return (
-    <main className="container">
-      <Switch>
-        <Route path="/movies" component={Movies}></Route>
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rentals" component={Rentals}></Route>
-        <Route path="/not-found" component={NotFound}></Route>
-        <Redirect from="/" to="/movies" exact />
-        <Redirect to="/not-found" />
-      </Switch>
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" to="/movies" exact />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
