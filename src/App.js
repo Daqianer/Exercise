1d0762b9e6c8c265cb76b7ed8029206364282b77
daqianer <daqianer@daqianerdeMacBook-Pro.local>
2021-01-18 15:07:07 +0800

    P105

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React from "react";
 import Movies from "./components/movies";
+import { Route, Redirect, Switch } from "react-router-dom";
+import "./App.css";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 
 function App() {
-  return <Movies />;
+  return (
+    <main className="container">
+      <Switch>
+        <Route path="/movies" component={Movies}></Route>
+        <Route path="/customers" component={Customers}></Route>
+        <Route path="/rentals" component={Rentals}></Route>
+        <Route path="/not-found" component={NotFound}></Route>
+        <Redirect from="/" to="/movies" exact />
+        <Redirect to="/not-found" />
+      </Switch>
+    </main>
+  );
 }
 
 export default App;
